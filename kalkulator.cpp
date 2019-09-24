3bda79e1599443a7edd33441923608fe6aae161e
grucjan031 <55709644+grucjan031@users.noreply.github.com>
2019-09-24 19:34:04 +0200

    Update kalkulator.cpp

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -1,5 +1,3 @@
-﻿
-#include "pch.h"
 #include <iostream>
 #include <Windows.h>
 #include <math.h>
@@ -7,51 +5,78 @@
 
 using namespace std;
 
-int a, b, wynik, menu;
+double a, b, wynik;
+bool repeat = true;
+int menu;
 
 
-int logarytm(int x , int y) {
-	wynik = (log(y) / log(x));
-	return wynik;
-}
+double oblicz(double x, double y) {
+	switch (menu) {
 
-int suma(int x, int y) {
-	wynik = x + y;
-	return wynik;
-}
+	case 1: {
 
-int roznica(int x, int y) {
-	wynik = x - y;
-	return wynik;
-}
-
-int potega(int x, int y) {
-
-	wynik = pow(x,y);
-	return wynik;
-}
-
-int dzielenie(int x, int y) {
-	wynik = x / y;
-	return wynik;
-}
-
-int mnozenie(int x, int y) {
-	wynik = x * y;
-	return wynik;
-}
-
-int silnia(int x) {
-	int tempx = 1;
-	for (int i = 1; i <=x; i++) {
-		tempx *= i;
+		wynik = (log(y) / log(x));
+		break;
 	}
-	wynik = tempx;
+	case 2: {
+		wynik = x + y;
+
+		break;
+	}
+	case 3: {
+		wynik = x - y;
+
+		break;
+	}
+	case 4: {
+		wynik = pow(x, y);
+
+		break;
+	}
+	case 5: {
+		wynik = x / y;
+
+		break;
+	}
+	case 6: {
+		wynik = x * y;
+
+		break;
+	}
+	case 7: {
+		double tempx = 1;
+		for (int i = 1; i <= x; i++) {
+			tempx *= i;
+		}
+		wynik = tempx;
+
+		break;
+	}
+	case 8: {
+		wynik = pow(x, 1 / y);
+
+		break;
+	}
+	case 9: {
+		if (x >= 0) {
+			wynik = x;
+		}
+		else {
+			wynik = x - (2 * x);
+		}
+
+		break;
+	}
+	}
+
+
 	return wynik;
 }
+
 
 void printmenu() {
-	cout << "\t Kalkulator liczb całkowitych który jest niespełna rozumu" << endl;
+	repeat = false;
+	cout << "\t Kalkulator który jest niespełna rozumu" << endl;
 	cout << "1 - Logarytm" << endl;
 	cout << "2 - Suma" << endl;
 	cout << "3 - Różnica" << endl;
@@ -59,124 +84,220 @@ void printmenu() {
 	cout << "5 - Dzielenie" << endl;
 	cout << "6 - Mnożenie" << endl;
 	cout << "7 - Silnia" << endl;
-}
-
-int main()
-{
-	setlocale(LC_ALL, "");
-	printmenu();
+	cout << "8 - Pierwiastkowanie" << endl;
+	cout << "9 - Wartość bezwzględna" << endl;
 
 	cout << "Wybierz równanie: ";
 	cin >> menu;
 	cout << endl;
 
 	switch (menu) {
+
+	case 1: {
+
+		cout << "Podaj podstawę logarytmu: ";
+		cin >> a;
+		cout << endl;
+		cout << "Podaj liczbę logarytmowaną: ";
+		cin >> b;
+		cout << endl;
+
+		oblicz(a, b);
+
+		cout << "Wynik to: " << wynik << endl;
+
+
+
+		break;
+	}
+	case 2: {
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << endl;
+		cout << "Podaj b: ";
+		cin >> b;
+		cout << endl;
+
+		oblicz(a, b);
+
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
+	case 3: {
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << endl;
+		cout << "Podaj b: ";
+		cin >> b;
+		cout << endl;
+
+		oblicz(a, b);
+
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
+	case 4: {
+		cout << "Podaj podstawę potęgi: ";
+		cin >> a;
+		cout << endl;
+		cout << "Podaj wykładnik: ";
+		cin >> b;
+		cout << endl;
+
+		oblicz(a, b);
+
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
+	case 5: {
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << endl;
+		cout << "Podaj b: ";
+		cin >> b;
+		cout << endl;
+
+		oblicz(a, b);
+
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
+	case 6: {
+		cout << "Podaj a: ";
+		cin >> a;
+		cout << endl;
+		cout << "Podaj b: ";
+		cin >> b;
+		cout << endl;
+
+		oblicz(a, b);
+
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
+	case 7: {
+		cout << "Podaj liczbę: ";
+		cin >> a;
+		cout << endl;
+
+		oblicz(a,0);
+
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
+	case 8: {
+		cout << "Podaj liczbę z której chcesz obliczyć pierwiastek: ";
+		cin >> a;
+		cout << endl;
+		cout << "Którego stopnia jest to pierwiastek: ";
+		cin >> b;
+		cout << endl;
+
+		oblicz(a, b);
+
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
+	case 9: {
+		cout << "Podaj liczbę z której chcesz obliczyć wartośc bezwzględną: ";
+		cin >> a;
+		cout << endl;
 	
-		case 1: {
-		
-			cout << "Podaj podstawę logarytmu: ";
-			cin >> a;
-			cout << endl;
-			cout << "Podaj liczbę logarytmowaną: ";
-			cin >> b;
-			cout << endl;
 
-			logarytm(a, b);
+		oblicz(a,0);
 
-			cout << "Wynik to: " << wynik << endl;
+		cout << "Wynik to: " << wynik << endl;
+
+		break;
+	}
 
 
-		
-			break;
-		}
-		case 2: {
-			cout << "Podaj a: ";
-			cin >> a;
-			cout << endl;
-			cout << "Podaj b: ";
-			cin >> b;
-			cout << endl;
-
-			suma(a, b);
-
-			cout << "Wynik to: " << wynik << endl;
-
-			break;
-		}
-		case 3: {
-			cout << "Podaj a: ";
-			cin >> a;
-			cout << endl;
-			cout << "Podaj b: ";
-			cin >> b;
-			cout << endl;
-
-			roznica(a, b);
-
-			cout << "Wynik to: " << wynik << endl;
-
-			break;
-		}
-		case 4: {
-			cout << "Podaj podstawę potęgi: ";
-			cin >> a;
-			cout << endl;
-			cout << "Podaj wykładnik: ";
-			cin >> b;
-			cout << endl;
-
-			potega(a, b);
-
-			cout << "Wynik to: " << wynik << endl;
-
-			break;
-		}
-		case 5: {
-			cout << "Podaj a: ";
-			cin >> a;
-			cout << endl;
-			cout << "Podaj b: ";
-			cin >> b;
-			cout << endl;
-
-			dzielenie(a, b);
-
-			cout << "Wynik to: " << wynik << endl;
-
-			break;
-		}
-		case 6: {
-			cout << "Podaj a: ";
-			cin >> a;
-			cout << endl;
-			cout << "Podaj b: ";
-			cin >> b;
-			cout << endl;
-
-			mnozenie(a, b);
-
-			cout << "Wynik to: " << wynik << endl;
-
-			break;
-		}
-		case 7: {
-			cout << "Podaj liczbę: ";
-			cin >> a;
-			cout << endl;
-
-			silnia(a);
-
-			cout << "Wynik to: " << wynik << endl;
-
-			break;
-		}
 
 
-			
 
 	}
+	system("PAUSE");
+	repeat = true;
+	system("cls");
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	while (repeat = true) {
+		printmenu();
+
+	}
+	
+
+	
+
 	system("PAUSE");
 	return 0;
 
 }
 
+/*
+double logarytm(double x , double y) {
+	wynik = (log(y) / log(x));
+	return wynik;
+}
+
+double suma(double x, double y) {
+	wynik = x + y;
+	return wynik;
+}
+
+double roznica(double x, double y) {
+	wynik = x - y;
+	return wynik;
+}
+
+double potega(double x, double y) {
+
+	wynik = pow(x,y);
+	return wynik;
+}
+
+double dzielenie(double x, double y) {
+	wynik = x / y;
+	return wynik;
+}
+
+double mnozenie(double x, double y) {
+	wynik = x * y;
+	return wynik;
+}
+
+double silnia(double x) {
+	double tempx = 1;
+	for (int i = 1; i <=x; i++) {
+		tempx *= i;
+	}
+	wynik = tempx;
+	return wynik;
+}
+
+double pierwiastkowanie(double x, double y) {
+
+	wynik = pow(x, 1/y);
+	return wynik;
+}
+double wartoscbezwzgledna(double x) {
+
+	if (x >= 0) {
+		wynik = x;
+	}
+	else {
+		wynik = x -(2 * x);
+	}
+	return wynik;
+}*/
